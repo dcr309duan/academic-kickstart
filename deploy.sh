f8382c1e745e3e5d6dc1eca44e8178433bf9b5cd
dcr <dcr309duan@gmail.com>
2019-05-18 12:25:59 +0800

    add travis config

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+set -e
+
+echo $GITHUB_AUTH_SECRET > ~/.git-credentials && chmod 0600 ~/.git-credentials
+git config --global credential.helper store
+git config --global user.email "dcr317duan@163.com"
+git config --global user.name "dcr-bot"
+git config --global push.default simple
+
+rm -rf deployment
+git clone -b master https://github.com/dcr309duan/dcr309duan.github.io.git deployment
+rsync -av --delete --exclude ".git" public/ deployment
+cd deployment
+git add -A
+git commit -m "rebuilding site on `date`, commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}" || true
+git push
+
+cd ..
+rm -rf deployment
